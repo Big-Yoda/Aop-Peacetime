1923dc35d87ec26d86dddcca4b2869a698968a7d
viggo95yoda <43821187+viggo95yoda@users.noreply.github.com>
2018-11-22 11:59:20 +0100

    Added Peacetime Sync Issue

--- server.lua
+++ server.lua
@@ -1,11 +1,9 @@
-print("AOP Script By Big Yoda")
-print("Feel Free to Edit and and reupload just credit Big Yoda or LaLord on the Fivem Community Website")
 
 local aop = "Sandy Shores/Harmony/Grapeseed"
 local s = ""
 
 RegisterCommand("aop", function(source, args, rawCommand)
-if IsPlayerAceAllowed(source, "yoda-aop") then
+if IsPlayerAceAllowed(source, "mgn-aop") then
     if args[1] ~= nil then
         aop = table.concat(args, " ")
         TriggerEvent("yodaaop:sync_sv")
@@ -23,21 +21,75 @@ end)
 RegisterServerEvent("yodaaop:sync_sv")
 AddEventHandler("yodaaop:sync_sv", function()
     TriggerClientEvent("yodaaop:sync_cl", -1, aop)
+end)
 
+RegisterServerEvent("yodapt:sync_sv")
+AddEventHandler("yodapt:sync_sv", function()
+    TriggerClientEvent("yoda:peacetime", -1, peacetimeon)
 end)
 
 peacetimeon = false
 
 RegisterCommand("peacetime", function(source, args, raw)
-if IsPlayerAceAllowed(source, "yoda-aop") then
+if IsPlayerAceAllowed(source, "mgn-aop") then
 	if peacetimeon == true then
 		peacetimeon = false
-		TriggerClientEvent("yoda:peacetime", -1, peacetimeon)
+		TriggerEvent("yodapt:sync_sv", peacetimeon)
 	elseif peacetimeon == false then
 		peacetimeon = true
-		TriggerClientEvent("yoda:peacetime", -1, peacetimeon)
+		TriggerEvent("yodapt:sync_sv", peacetimeon)
 	end
 else
 	TriggerClientEvent("chatMessage", source, "^*^1Yoda AOP: ^3Insufficient Permission", {255, 255, 255})
 end
+end)
+
+RegisterCommand("cooldown", function(source, args, raw)
+if IsPlayerAceAllowed(source, "mgn-aop") then
+	local time = tonumber(args[1])
+	if time == nil or time == "" then
+		TriggerEvent("yoda:cooldown_sv", 15)
+	elseif time > 30 then
+		TriggerEvent("yoda:cooldown_sv", 30)
+	else
+		if timeron then
+			TriggerClientEvent('chatMessage', source, "^1Cooldown", {255, 255, 255}, "^3The timer is already on.")
+		else
+			TriggerEvent("yoda:cooldown_sv", time)
+		end
+	end
+else
+	TriggerClientEvent("chatMessage", source, "^*^1Yoda AOP: ^3Insufficient Permission", {255, 255, 255})
+end
+end)
+
+RegisterCommand("reset", function(source, args, raw)
+if IsPlayerAceAllowed(source, "mgn-aop") then
+	second = 0
+	minute = 0
+else
+	TriggerClientEvent("chatMessage", source, "^*^1Yoda AOP: ^3Insufficient Permission", {255, 255, 255})
+end
+end)
+	
+
+RegisterServerEvent("yoda:cooldown_sv")
+AddEventHandler("yoda:cooldown_sv", function(time)
+	second = 0
+	minute = time
+	timeron = true
+	TriggerClientEvent("yoda:cooldown_cl", -1, minute, second)
+	TriggerClientEvent('chatMessage', -1, "^1^*ANNOUNCEMENT", {255, 255, 255}, "Priority Cooldown is now in effect for the next "..time.."Minute(s).")
+	while timeron do
+		Citizen.Wait(1000)
+		if minute == 0 and second == 0 and timeron then
+			timeron = false
+			TriggerClientEvent('chatMessage', -1, "^1^*ANNOUNCEMENT", {255, 255, 255}, "Resume normal RP")
+		elseif second == -1 then
+			minute = minute - 1
+			second = 59
+		end
+		TriggerClientEvent("yoda:cooldown_cl", -1, minute, second)
+		second = second - 1
+	end
 end)
